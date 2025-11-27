7d02dedff16ad96497764da4a4e5541979446793
Nghiem Vy Son <nghiemvyson@gmail.com>
2025-11-27 21:29:52 +0700

    toi uu UI cap nhat Courseitem them MycourseAdapter va Update MycourseFragment

--- app/src/main/java/vn/edu/usth/classroomschedulemanagementapp/MyCoursesFragment.java
+++ app/src/main/java/vn/edu/usth/classroomschedulemanagementapp/MyCoursesFragment.java
@@ -1,12 +1,23 @@
 package vn.edu.usth.classroomschedulemanagementapp;
 
 import android.os.Bundle;
+
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MyCoursesFragment extends Fragment {
+
+    RecyclerView rcvCourse;
+    MyCoursesAdapter adapter;
+    List<String> courseList = new ArrayList<>();
 
     public MyCoursesFragment() {
         // Required empty public constructor
@@ -15,7 +26,21 @@ public class MyCoursesFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.my_course, container, false);
+
+        View view = inflater.inflate(R.layout.my_course, container, false);
+        rcvCourse = view.findViewById(R.id.rcvCourse);
+
+        // Fake data
+        courseList.add("Calculus 1");
+        courseList.add("Mobile Development");
+        courseList.add("Computer Network");
+        courseList.add("AI Fundamentals");
+
+        adapter = new MyCoursesAdapter(courseList, "Detail");
+
+        rcvCourse.setLayoutManager(new LinearLayoutManager(getContext()));
+        rcvCourse.setAdapter(adapter);
+
+        return view;
     }
 }
